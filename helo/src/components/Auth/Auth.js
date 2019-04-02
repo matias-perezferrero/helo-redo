e7e4234bd2aacdcc6ebfe8ac102f81969813da0c
Matias Perez <matias.perezferrero@gmail.com>
2019-04-02 11:43:00 -0600

    setting up component structure

---
+++ helo/src/components/Auth/Auth.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Auth extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Auth Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Auth;
---
+++ helo/src/components/Form/Form.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Form Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Form;
